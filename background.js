16bcf31c732d415db49ef4e714d24fe8c672808a
Muhammed Emin Arayıcı <72305188+Meminseeker@users.noreply.github.com>
2023-01-03 00:50:28 +0300

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'password-generated') {
-        const { password } = request;
-
-        sendResponse({ status: 'success' })
-    }
-});
